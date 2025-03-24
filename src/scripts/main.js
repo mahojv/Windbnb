c9d696cec9af3b5850882355237e8064fb464dab
mahojv <moisesmjavier@outlook.com>
2025-03-23 18:18:15 -0600

    carga de tarjetas automatica en utils. buscador toggle (apertura y cierre

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,47 @@
  * Este bloque de código contiene la funcionalidad principal
  * que define el comportamiento del programa.
  */
+
+import {stays} from "./stays.js";
+import { loadStays } from "./utils.js";
+
+const buscadorBtns = document.querySelectorAll("#buscadorBtns")
+const locationSearch = document.querySelector("#locationSearch")
+const guestSearch = document.querySelector("#guestSearch")
+const searchBtn = document.querySelector("#searchBtn")
+const closeTogleBtn = document.querySelector("#closeTogleBtn")
+
+const cardsContainer = document.querySelector("#cardsContainer")
+const toggleSearch = document.querySelector("#toggleSearch")
+
+locationSearch.addEventListener('click', function(x){
+    toggleSearch.classList.remove("hidden")
+
+    }
+)
+guestSearch.addEventListener('click',function(x){
+    toggleSearch.classList.remove("hidden")
+
+    } )
+searchBtn.addEventListener('click', function(x){
+    toggleSearch.classList.remove("hidden")
+
+    })
+
+    closeTogleBtn.addEventListener('click', function(x){
+
+        toggleSearch.classList.add("hidden")
+
+    })
+
+
+    buscadorBtns.forEach(function (event){
+
+        console.log(event.tagName) 
+
+    })
+
+console.log("fh")
+
+loadStays(stays, cardsContainer )
+
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,5 +1,60 @@
-/**
- * Módulo de funciones utilitarias.
- * Este archivo contiene funciones auxiliares que serán utilizadas y llamadas
- * desde el archivo principal para realizar varias operaciones.
- */
+
+
+
+export function loadStays (array, box){
+    
+box.innerHTML = ""
+array.forEach(item => {
+
+    let superHostItem = ""
+    let bedsItem = 
+            `${item.beds} beds`
+
+
+
+
+        if(item.superHost === true){
+            superHostItem = `
+            <p class="flex border h-[34px] min-w-[100px] justify-center items-center rounded-xl px-[5px] font-extrabold text-[12px]">
+    SUPER HOST</p>`
+        
+        }
+        if(item.beds === null){
+            bedsItem =      ""   
+
+        }
+
+    const template = `
+
+            <div role="card" class="flex gap-1 flex-col max-w-[600px] max-h-[460px] sm:max-w-[283px] md:max-w-[380px]">
+                <figure class="rounded-xl overflow-hidden max-h-[252px] w-full ">
+                    <img class="object-cover aspect-video" src=${item.photo}
+                        alt="">
+                </figure>
+                <span class="flex justify-between ">
+                    <span class=" flex items-center  gap-3 ">
+                        ${superHostItem}
+                        <p class="text-grisTexto text-[14px]">${item.type}. ${bedsItem} </p>
+                    </span>
+                    <span class="flex gap-2 w-fit items-center ">
+                        <figure class="size-6 text-rojoclaro ">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5"
+                                fill="currentColor" stroke="currentColor" class="  ">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                            </svg>
+                        </figure>
+                        <p class="text-grisRanking text-[15px]">${item.rating}</p>
+                    </span>
+                </span>
+                <p>${item.title} </p>
+
+
+            </div> `
+
+         box.innerHTML += template
+
+});
+
+
+}
