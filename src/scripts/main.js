cda99ca32772079ae4d6999459fef34e5d46a089
mahojv <moisesmjavier@outlook.com>
2025-03-26 17:15:16 -0600

    se corrigen errores y se limpia codigo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,14 @@
  */
 
 import { stays } from "./stays.js";
-import { loadStays, sumador, totalCounterGuest, filterSearch, listaPrincipal, suggestionDeploy } from "./utils.js";
+import {
+  loadStays,
+  sumador,
+  totalCounterGuest,
+  filterSearch,
+  listaPrincipal,
+  suggestionDeploy,
+} from "./utils.js";
 
 // botones
 const searchBarBtns = document.querySelectorAll("#searchBarBtns");
@@ -14,40 +21,36 @@ const guestSearch = document.querySelector("#guestSearch");
 const searchBtn = document.querySelector("#searchBtn");
 const closeTogleBtn = document.querySelector("#closeTogleBtn");
 const counterAdult = document.querySelectorAll("#counterAdult");
-const counterChildren = document.querySelectorAll("#counterChildren")
-const searchStay = document.querySelector("#searchStay")
-const searchStay2 = document.querySelector("#searchStay2")
-
-
+const counterChildren = document.querySelectorAll("#counterChildren");
+const searchStay = document.querySelector("#searchStay");
+const searchStay2 = document.querySelector("#searchStay2");
 
 // contenedores y etiquetas
 const cardsContainer = document.querySelector("#cardsContainer");
 const toggleSearch = document.querySelector("#toggleSearch");
-const counterAdultTag = document.querySelector("#counterAdultTag")
-const counterChildTag = document.querySelector("#counterChildTag")
-const guestInputSearch = document.querySelector("#guestInputSearch")
-const locationInputSearch = document.querySelector("#locationInputSearch")
-const placeSugestions = document.querySelector("#placeSugestions")
-const guestCounter = document.querySelector("#guestCounter")
-const itemsCardCounter = document.querySelector("#itemsCardCounter")
-const sugerenciasBox = document.querySelector("#sugerenciasBox")
-const locationPlaceBox = document.querySelector("#locationPlaceBox")
+const counterAdultTag = document.querySelector("#counterAdultTag");
+const counterChildTag = document.querySelector("#counterChildTag");
+const guestInputSearch = document.querySelector("#guestInputSearch");
+const locationInputSearch = document.querySelector("#locationInputSearch");
+const placeSugestions = document.querySelector("#placeSugestions");
+const guestCounter = document.querySelector("#guestCounter");
+const itemsCardCounter = document.querySelector("#itemsCardCounter");
+const sugerenciasBox = document.querySelector("#sugerenciasBox");
+const locationPlaceBox = document.querySelector("#locationPlaceBox");
 
 // Variables Globales
-let contadorA = 0
-let contadorC = 0
-let contadorTotal = 0
-let contentInputSearch = "Whole, Finland"
-let contentGuestSearch = ""
-let reduccionSugerencias = []
-let principalArray = []
-let arrayInput = ""
-let nuevaLista = ""
+let contadorA = 0;
+let contadorC = 0;
+let contadorTotal = 0;
+let contentInputSearch = "Whole, Finland";
+let contentGuestSearch = "";
+let reduccionSugerencias = [];
+let principalArray = [];
+let arrayInput = "";
+let nuevaLista = "";
 
-locationSearch.textContent = contentInputSearch
-guestSearch.textContent = `Add Guest`
-
-
+locationSearch.textContent = contentInputSearch;
+guestSearch.textContent = `Add Guest`;
 
 locationSearch.addEventListener("click", function (x) {
   toggleSearch.classList.remove("hidden");
@@ -64,258 +67,153 @@ closeTogleBtn.addEventListener("click", function (x) {
 });
 
 searchBarBtns.forEach(function (event) {
+  event.addEventListener("click", function (x) {
+    let codigo = x.target;
+    let etiqueta = codigo.id;
 
+    if (
+      etiqueta === "locationLabelSearch" ||
+      etiqueta === "locationInputSearch"
+    ) {
+      principalArray = listaPrincipal(stays);
+      suggestionDeploy(principalArray, sugerenciasBox);
 
-    event.addEventListener('click', function (x){
-        let codigo = x.target
-        let etiqueta = codigo.id
-        let locationValue = locationInputSearch.value
-        
-        
-
-        if(etiqueta === "locationLabelSearch" || etiqueta === "locationInputSearch"){
-
-
-            // locationValue = codigo.value
-
-            principalArray = listaPrincipal(stays)
-            suggestionDeploy(principalArray, sugerenciasBox)
-            
-
-
-
-            placeSugestions.classList.remove("hidden")
-            guestCounter.classList.add("hidden")
-        } else if(etiqueta === "guestLabelSearch" || etiqueta === "guestInputSearch"){
-
-            placeSugestions.classList.add("hidden")
-            guestCounter.classList.remove("hidden")
-
-        } 
-        // else if(etiqueta === "searchStay"){
-             
-        //     console.log(locationValue)
-            
-
-
-        // }
-
-
-    })
-  
+      placeSugestions.classList.remove("hidden");
+      guestCounter.classList.add("hidden");
+    } else if (
+      etiqueta === "guestLabelSearch" ||
+      etiqueta === "guestInputSearch"
+    ) {
+      placeSugestions.classList.add("hidden");
+      guestCounter.classList.remove("hidden");
+    }
+  });
 });
 
-counterAdult.forEach(function(event){
+counterAdult.forEach(function (event) {
+  event.addEventListener("click", function (x) {
+    let codigo = x.target;
+    let etiqueta = codigo.tagName;
 
-
-    
-    event.addEventListener('click', function (x){
-        let codigo = x.target
-        let etiqueta = codigo.tagName
-
-        if(etiqueta === "BUTTON"){
-            contadorA =  sumador(contadorA, codigo.value, counterAdultTag)
-            contadorTotal =  totalCounterGuest(contadorA, contadorC, guestInputSearch)
-   
-        }
-    })
-})
-
-counterChildren.forEach(function(event){
-
-    event.addEventListener('click', function (x){
-        let codigo = x.target
-        let etiqueta = codigo.tagName
-
-        if(etiqueta === "BUTTON"){
-            contadorC = sumador(contadorC, codigo.value, counterChildTag)         
-            contadorTotal = totalCounterGuest(contadorA, contadorC, guestInputSearch)
-    
-        }
-    })
-    
-})
-
-locationInputSearch.addEventListener('input', function(event){
-
-    contentInputSearch = event.target.value
-
-        principalArray = listaPrincipal(stays)
-            suggestionDeploy(principalArray, sugerenciasBox)
-
-             arrayInput = contentInputSearch.split(", ")
-    
-
-
-    console.log(arrayInput)
-    nuevaLista = filterSearch(arrayInput,contadorTotal, stays)
-
-
-    
-
-
-    if(nuevaLista.length > 0){
-        
-        console.log(typeof(arrayInput.length))
-
-        loadStays(nuevaLista, cardsContainer, itemsCardCounter);
-
-        if(arrayInput.length === 1 ){
-        
-        locationSearch.textContent = `${contentInputSearch}, Finland` 
-        guestSearch.textContent = `${contadorTotal} guests`
-
-        }else if(arrayInput.length === 2 ){
-
-            locationSearch.textContent = contentInputSearch 
-            guestSearch.textContent = `${contadorTotal} guests`
-    
-            }
-
-        
-            
-        
-
+    if (etiqueta === "BUTTON") {
+      contadorA = sumador(contadorA, codigo.value, counterAdultTag);
+      contadorTotal = totalCounterGuest(contadorA, contadorC, guestInputSearch);
     }
+  });
+});
 
-    
+counterChildren.forEach(function (event) {
+  event.addEventListener("click", function (x) {
+    let codigo = x.target;
+    let etiqueta = codigo.tagName;
+
+    if (etiqueta === "BUTTON") {
+      contadorC = sumador(contadorC, codigo.value, counterChildTag);
+      contadorTotal = totalCounterGuest(contadorA, contadorC, guestInputSearch);
+    }
+  });
+});
+
+locationInputSearch.addEventListener("input", function (event) {
+  contentInputSearch = event.target.value;
+
+  principalArray = listaPrincipal(stays);
+  suggestionDeploy(principalArray, sugerenciasBox);
+
+  arrayInput = contentInputSearch.split(", ");
+
+  nuevaLista = filterSearch(arrayInput, contadorTotal, stays);
+
+  if (nuevaLista.length > 0) {
+    console.log(typeof arrayInput.length);
+
+    loadStays(nuevaLista, cardsContainer, itemsCardCounter);
+
+    if (arrayInput.length === 1) {
+      locationSearch.textContent = `${contentInputSearch}, Finland`;
+      guestSearch.textContent = `${contadorTotal} guests`;
+    } else if (arrayInput.length === 2) {
+      locationSearch.textContent = contentInputSearch;
+      guestSearch.textContent = `${contadorTotal} guests`;
+    }
+  }
+
+  locationPlaceBox.innerHTML = `
+        <h1 id="locationPlaceBox" class="text-2xl font-bold">Stays in ${contentInputSearch}</h1>
+        `;
+});
+
+guestInputSearch.addEventListener("input", function (event) {
+  contentGuestSearch = event.target.value;
+  console.log(contentGuestSearch);
+
+  nuevaLista = filterSearch(arrayInput, contadorTotal, stays);
+  console.log(nuevaLista);
+
+  if (nuevaLista.length > 0) {
+    loadStays(nuevaLista, cardsContainer, itemsCardCounter);
+
     locationPlaceBox.innerHTML = `
         <h1 id="locationPlaceBox" class="text-2xl font-bold">Stays in ${contentInputSearch}</h1>
-        `
-    
-})
+        `;
+    locationSearch.textContent = contentInputSearch;
+    guestSearch.textContent = `${contadorTotal} guests`;
+  }
+});
 
+searchStay.addEventListener("click", function (x) {
+  if (arrayInput.length === 1) {
+    locationSearch.textContent = `${contentInputSearch}, Finland`;
+    guestSearch.textContent = `${contadorTotal} guests`;
+  } else if (arrayInput.length === 2) {
+    locationSearch.textContent = contentInputSearch;
+    guestSearch.textContent = `${contadorTotal} guests`;
+  }
+  nuevaLista = filterSearch(arrayInput, contadorTotal, stays);
 
+  if (nuevaLista.length > 0) {
+    loadStays(nuevaLista, cardsContainer, itemsCardCounter);
 
-
-guestInputSearch.addEventListener('input', function(event){
-
-    contentGuestSearch = event.target.value
-    console.log(contentGuestSearch)
-    
-
-    nuevaLista = filterSearch(arrayInput, contadorTotal, stays)
-    console.log(nuevaLista)
-
-    if(nuevaLista.length > 0){
-
-        loadStays(nuevaLista, cardsContainer, itemsCardCounter);
-
-      
-        locationPlaceBox.innerHTML = `
+    locationPlaceBox.innerHTML = `
         <h1 id="locationPlaceBox" class="text-2xl font-bold">Stays in ${contentInputSearch}</h1>
-        `
-            locationSearch.textContent = contentInputSearch
-            guestSearch.textContent = `${contadorTotal} guests`
-    }
-
-
-})
-
-
-searchStay.addEventListener('click', function (x){
-
-
-    if(arrayInput.length === 1 ){
-        
-        locationSearch.textContent = `${contentInputSearch}, Finland` 
-        guestSearch.textContent = `${contadorTotal} guests`
-
-        
-
-        }else if(arrayInput.length === 2 ){
-
-            locationSearch.textContent = contentInputSearch 
-            guestSearch.textContent = `${contadorTotal} guests`
-    
-            }
-
-
-    
-
-    nuevaLista = filterSearch(arrayInput ,contadorTotal, stays)
-
-
-    if(nuevaLista.length > 0){
-
-        loadStays(nuevaLista, cardsContainer, itemsCardCounter);
-
-        locationPlaceBox.innerHTML = `
-        <h1 id="locationPlaceBox" class="text-2xl font-bold">Stays in ${contentInputSearch}</h1>
-        `
-        
-    }else{
-        console.log("cero destinos")
-        cardsContainer.innerHTML = ""
-        locationPlaceBox.innerHTML = `
+        `;
+  } else {
+    console.log("cero destinos");
+    cardsContainer.innerHTML = "";
+    locationPlaceBox.innerHTML = `
         <h1 id="locationPlaceBox" class="text-2xl font-bold">Sin resultados</h1>
-        `
-        itemsCardCounter.innerHTML = `<p id="itemsCardCounter" class="font-semibold">0 stays</p>`
-    }
+        `;
+    itemsCardCounter.innerHTML = `<p id="itemsCardCounter" class="font-semibold">0 stays</p>`;
+  }
+  toggleSearch.classList.add("hidden");
+});
 
+searchStay2.addEventListener("click", function (x) {
+  if (arrayInput.length === 1) {
+    locationSearch.textContent = `${contentInputSearch}, Finland`;
+    guestSearch.textContent = `${contadorTotal} guests`;
+  } else if (arrayInput.length === 2) {
+    locationSearch.textContent = contentInputSearch;
+    guestSearch.textContent = `${contadorTotal} guests`;
+  }
 
-    toggleSearch.classList.add("hidden");
-    
+  nuevaLista = filterSearch(arrayInput, contadorTotal, stays);
 
-    
+  if (nuevaLista.length > 0) {
+    loadStays(nuevaLista, cardsContainer, itemsCardCounter);
 
-
-
-})
-
-searchStay2.addEventListener('click', function (x){
-
-    if(arrayInput.length === 1 ){
-        
-        locationSearch.textContent = `${contentInputSearch}, Finland` 
-        guestSearch.textContent = `${contadorTotal} guests`
-
-        }else if(arrayInput.length === 2 ){
-
-            locationSearch.textContent = contentInputSearch 
-            guestSearch.textContent = `${contadorTotal} guests`
-    
-            }
-    
-
-    nuevaLista = filterSearch(arrayInput ,contadorTotal, stays)
-    console.log(nuevaLista)
-
-    if(nuevaLista.length > 0){
-
-        loadStays(nuevaLista, cardsContainer, itemsCardCounter);
-
-        locationPlaceBox.innerHTML = `
+    locationPlaceBox.innerHTML = `
         <h1 id="locationPlaceBox" class="text-2xl font-bold">Stays in ${contentInputSearch}</h1>
-        `
-
-    }else{
-        console.log("cero destinos")
-
-        cardsContainer.innerHTML = ""
-        locationPlaceBox.innerHTML = `
+        `;
+  } else {
+    cardsContainer.innerHTML = "";
+    locationPlaceBox.innerHTML = `
         <h1 id="locationPlaceBox" class="text-2xl font-bold">Sin resultados</h1>
-        `
-        itemsCardCounter.innerHTML = `<p id="itemsCardCounter" class="font-semibold">0 stays</p>`
-    }
+        `;
+    itemsCardCounter.innerHTML = `<p id="itemsCardCounter" class="font-semibold">0 stays</p>`;
+  }
 
-    toggleSearch.classList.add("hidden");
-
-    // if(arrayInput.length = 1 ){
-
-    //     locationSearch.textContent = `${contentInputSearch}, Finland` 
-    //     guestSearch.textContent = `${contadorTotal} guests`
-
-
-    //     }else{
-
-    //         locationSearch.textContent = `${contentInputSearch}` 
-    //         guestSearch.textContent = `${contadorTotal} guests`
-    //         }
-
-
-
-})
-
+  toggleSearch.classList.add("hidden");
+});
 
 loadStays(stays, cardsContainer, itemsCardCounter);
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,56 +1,42 @@
+/**
+ * Description Carga de cards a contenedor
+ *
+ * @export
+ * @param {array} array : array a buscar
+ * @param {html} box : contenedor de tarjetas
+ * @param {html} itemsBox : contenedor de cantidad de items
+ */
+export function loadStays(array, box, itemsBox) {
+  box.innerHTML = "";
+  itemsBox.innerHTML = "";
 
+  let items = array.length;
+  let itemsTemplate = "";
 
-
-export function loadStays (array, box, itemsBox){
-    
-box.innerHTML = ""
-itemsBox.innerHTML = ""
-
-
-let items = array.length
-let itemsTemplate = ""
-
-
-let locationTemplate = `
+  let locationTemplate = `
 <h1 id="locationPlaceBox" class="text-2xl font-bold">Stays in finland</h1>
-`
+`;
 
-if(items > 12){
+  if (items > 12) {
+    itemsTemplate = `<p id="itemsCardCounter" class="font-semibold">12+ stays</p>`;
+  } else if (items > 1 || items === 0) {
+    itemsTemplate = `<p id="itemsCardCounter" class="font-semibold">${items} stays</p>`;
+  } else {
+    itemsTemplate = `<p id="itemsCardCounter" class="font-semibold">${items} stay</p>`;
+  }
 
-     itemsTemplate = `<p id="itemsCardCounter" class="font-semibold">12+ stays</p>`
+  array.forEach((item) => {
+    let superHostItem = "";
+    let bedsItem = `${item.beds} beds`;
 
-}else if(items > 1 || items === 0){
-    
-    itemsTemplate = `<p id="itemsCardCounter" class="font-semibold">${items} stays</p>`
-
-}else{
-
-    itemsTemplate = `<p id="itemsCardCounter" class="font-semibold">${items} stay</p>`
-
-
-}
-
-
-
-array.forEach(item => {
-
-    let superHostItem = ""
-    let bedsItem = 
-            `${item.beds} beds`
-
-
-
-
-        if(item.superHost === true){
-            superHostItem = `
+    if (item.superHost === true) {
+      superHostItem = `
             <p class="flex border h-[34px] min-w-[100px] justify-center items-center rounded-xl px-[5px] font-extrabold text-[12px]">
-    SUPER HOST</p>`
-        
-        }
-        if(item.beds === null){
-            bedsItem =      ""   
-
-        }
+    SUPER HOST</p>`;
+    }
+    if (item.beds === null) {
+      bedsItem = "";
+    }
 
     const template = `
 
@@ -78,162 +64,143 @@ array.forEach(item => {
                 <p>${item.title} </p>
 
 
-            </div> `
+            </div> `;
 
-         box.innerHTML += template
-         itemsBox.innerHTML = itemsTemplate
-
-
-});
-
-
+    box.innerHTML += template;
+    itemsBox.innerHTML = itemsTemplate;
+  });
 }
 
-
-export function sumador (counter, op, tag){
-    const idIndividual = tag.id
-    if (op === "-" && counter > 0){
-        counter-= 1 
-
-
-   }else if (op === "+"){
-
-         counter+= 1
-    }
-    const template = `
+/**
+ * Description Sumador de operadores
+ *
+ * @export
+ * @param {number} counter
+ * @param {string} op
+ * @param {html} tag
+ */
+export function sumador(counter, op, tag) {
+  const idIndividual = tag.id;
+  if (op === "-" && counter > 0) {
+    counter -= 1;
+  } else if (op === "+") {
+    counter += 1;
+  }
+  const template = `
         <p id="${idIndividual}" >${counter}</p>
-        `
-    tag.innerHTML= template
+        `;
+  tag.innerHTML = template;
 
-    return counter
+  return counter;
 }
 
-export function totalCounterGuest(cA, cB, tag){
+/**
+ * Description Contador de total de huespedes
+ *
+ * @export
+ * @param {number} cA Contador de Adultos
+ * @param {number} cB Contador de Niños
+ * @param {html} tag  impresion a html
+ */
+export function totalCounterGuest(cA, cB, tag) {
+  const cT = cA + cB;
 
-    const cT = cA + cB
+  tag.innerHTML = `<p class="text-center" id="guestInputSearch">${cT}</p>`;
 
-    tag.innerHTML= `<p class="text-center" id="guestInputSearch">${cT}</p>`
-    
-    return cT
-
-
+  return cT;
 }
 
-export function filterSearch(contentInputSearch, guestSearch , arrayStay) {
-
-    let arrayFiltered = arrayStay.filter(function (search){
-
-        if(contentInputSearch === ""){
-
-            if(search.maxGuests >= guestSearch){
-
-                return search
-            }
-
-
-        }else if(contentInputSearch.length === 1) {
-            if(contentInputSearch[0].toLowerCase().includes(search.city.toLowerCase()) && search.maxGuests >= guestSearch ){
-
-                return search
-                
-    
-            }
-
-        } else if(contentInputSearch.length === 2) {
-            if(contentInputSearch[0].toLowerCase().includes(search.city.toLowerCase()) && contentInputSearch[1].toLowerCase().includes(search.country.toLowerCase()) && search.maxGuests >= guestSearch ){
-
-                return search
-                
-    
-            }
-        
-
-
-
-        
-
-        
-
-    
-
+/**
+ * Description Filtro de busqueda
+ *
+ * @export
+ * @param {array} contentInputSearch entrada de valores String en array para busqueda
+ * @param {number} guestSearch entrada de contador de huespedes
+ * @param {array} arrayStay array principal
+ */
+export function filterSearch(contentInputSearch, guestSearch, arrayStay) {
+  let arrayFiltered = arrayStay.filter(function (search) {
+    if (contentInputSearch === "") {
+      if (search.maxGuests >= guestSearch) {
+        return search;
+      }
+    } else if (contentInputSearch.length === 1) {
+      if (
+        contentInputSearch[0]
+          .toLowerCase()
+          .includes(search.city.toLowerCase()) &&
+        search.maxGuests >= guestSearch
+      ) {
+        return search;
+      }
+    } else if (contentInputSearch.length === 2) {
+      if (
+        contentInputSearch[0]
+          .toLowerCase()
+          .includes(search.city.toLowerCase()) &&
+        contentInputSearch[1]
+          .toLowerCase()
+          .includes(search.country.toLowerCase()) &&
+        search.maxGuests >= guestSearch
+      ) {
+        return search;
+      }
     }
-}
-    
-
-)
-
-
-return arrayFiltered
-        
-
-
-    
+  });
+  return arrayFiltered;
 }
 
-export function listaPrincipal(array){
+/**
+ * Description generador de valores para lista de sugerencias con ciudad y pais
+ *
+ * @export
+ * @param {array} array array principal
+ */
+export function listaPrincipal(array) {
+  let nuevoArray = array.map(function (x) {
+    let arrayRecorte = Object.values(x).slice(0, 2);
 
-    let nuevoArray = array.map(function(x){
+    return arrayRecorte;
+  });
 
-        let arrayRecorte = Object.values(x).slice(0,2)
-
-        return arrayRecorte
-
-
-    })
-
-    return nuevoArray
-
+  return nuevoArray;
 }
 
-export function suggestionDeploy (array, suggestionBox){
-
-const reduccion = array.reduce(function (acum, item){
-
-
-    const [ciudad, pais] = item
-
+/**
+ * Description generador de valores para lista de sugerencias
+ *
+ * @export
+ * @param {array} array array con valores nuevos unicamente con ciudad y pais
+ * @param {html} suggestionBox  contenedor de lista
+ */
+export function suggestionDeploy(array, suggestionBox) {
+  const reduccion = array.reduce(function (acum, item) {
+    const [ciudad, pais] = item;
 
     let existe = acum.some(function (x) {
-        const ciudadAcum = x[0]
-        const paisAcum = x[1]
+      const ciudadAcum = x[0];
+      const paisAcum = x[1];
 
-
-        return ciudadAcum === ciudad && paisAcum === pais
+      return ciudadAcum === ciudad && paisAcum === pais;
     });
 
     if (!existe) {
-        acum.push(item)
+      acum.push(item);
     }
 
-    return acum
-}, [])
+    return acum;
+  }, []);
 
-// return reduccion
+  suggestionBox.innerHTML = ``;
 
-suggestionBox.innerHTML= ``
-
-reduccion.forEach(function(x){
-
+  reduccion.forEach(function (x) {
     const template = `    
     <span class="flex gap-1.5 items-center ">
                     <img class="size-5 " src="./src/images/icons/pin.svg" alt="">
                     <p class="text-grisRanking">${x[0]}, ${x[1]}</p>
                 </span>
-                `
+                `;
 
-    
-    
-
-
-    suggestionBox.innerHTML += template
-})
-
-
-
-
-
-
-
-
+    suggestionBox.innerHTML += template;
+  });
 }
-
